52cc5f524d7373fa28248e4a3a0f149b63cd1419
shaokaiye <shaokaiyeah@gmail.com>
2024-06-18 14:34:01 +0200

    fixed the duplicate plot

--- amadeusgpt/__init__.py
+++ amadeusgpt/__init__.py
@@ -7,5 +7,19 @@ Apache-2.0 license
 
 from amadeusgpt.implementation import AnimalBehaviorAnalysis
 from amadeusgpt.main import AMADEUS
-
 from amadeusgpt.version import __version__, VERSION
+from integration_modules import *
+from matplotlib import pyplot as plt
+params = {
+    "axes.labelsize": 10,
+    "legend.fontsize": 10,
+    "xtick.labelsize": 10,
+    "ytick.labelsize": 10,
+    "text.usetex": False,
+    "figure.figsize": [8, 8],
+    "font.size": 10,
+}
+plt.rcParams.update(params)
+plt.rcParams['figure.subplot.hspace'] = 0.5  # Horizontal space between subplots
+plt.rcParams['figure.subplot.wspace'] = 0.5  # Vertical space between subplots
+plt.style.use('dark_background')    
