456ee9e7ddeda2d49d37cfe0408961ddae1df15c
shaokaiye <shaokaiyeah@gmail.com>
2024-07-06 19:54:22 +0200

    cleaned config files

--- amadeusgpt/managers/model_manager.py
+++ amadeusgpt/managers/model_manager.py
@@ -1,15 +1,11 @@
-from typing import List
-
 from .base import Manager
 
-
 class ModelManager(Manager):
+    """
+    Not implemented yet. Perhaps we don't need a model manager.
+    """
     def __init__(self, config):
-        self.config = config
-        self.sam_config = config["sam_info"]
-        self.sa_config = config["keypoint_info"]
-        self.init_SA_model()
-        self.init_SAM_model()
+        self.config = config     
 
     def init_SA_model(self):
         pass
