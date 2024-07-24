725d7a7ce887d1021c278373d2cefda105049893
shaokaiye <shaokaiyeah@gmail.com>
2024-07-24 10:13:32 +0200

    added test demo test

---
+++ tests/test_demo_data.py
@@ -0,0 +1,24 @@
+import os
+import amadeusgpt
+from pathlib import Path
+from amadeusgpt.config import Config
+from amadeusgpt import AMADEUS
+import pytest
+
+
+@pytest.mark.parametrize("template_name", ["EPM_template.yaml", "MABe_template.yaml", "Horse_template.yaml", "MausHaus_template.yaml"])
+def test_demo_data(template_name):
+    # test the completeness of the demo data
+    amadeus_root = Path(amadeusgpt.__file__).parent.parent
+    config = Config(amadeus_root / f"amadeusgpt" / "configs" / template_name)
+    config['data_info']['data_folder'] = amadeus_root / config['data_info']['data_folder']
+    amadeus = AMADEUS(config, use_vlm = False)
+    video_file_paths = amadeus.get_video_file_paths()
+    keypoint_file_paths = amadeus.get_keypoint_file_paths()
+    assert len(video_file_paths) == 1
+    assert len(keypoint_file_paths) == 1
+    assert os.path.exists(os.path.join(config['data_info']['data_folder'], 'example.json'))
+
+
+
+
