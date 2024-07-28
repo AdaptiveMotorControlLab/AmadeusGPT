1591278deffd78e04304fdc3dc3a569859e8b2ae
shaokaiye <shaokaiyeah@gmail.com>
2024-07-28 15:54:45 +0200

    updated readme

--- tests/test_project_creation.py
+++ tests/test_project_creation.py
@@ -1,9 +1,11 @@
 import os
+from amadeusgpt import create_project
+from amadeusgpt import AMADEUS
+from amadeusgpt.utils import parse_result
+
 if 'OPENAI_API_KEY' not in os.environ:  
      os.environ['OPENAI_API_KEY'] = 'your key'
 
-from amadeusgpt import create_project
-from amadeusgpt import AMADEUS
 # Create a project
 
 data_folder = "temp_data_folder"
@@ -13,3 +15,7 @@ config = create_project(data_folder, result_folder)
 
 # Create an AMADEUS instance
 amadeus = AMADEUS(config)
+
+# query = "Plot the trajectory of the animal using the animal center and color it by time"
+# qa_message = amadeus.step(query)
+# parse_result(amadeus, qa_message)
