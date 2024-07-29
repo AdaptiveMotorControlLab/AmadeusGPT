31408a88f19776dc02ddeb98de2b53093e6c4ee8
shaokaiye <shaokaiyeah@gmail.com>
2024-07-29 16:35:37 +0200

    corrected broken links and broken references

--- conda/install_minimal.sh
+++ conda/install_minimal.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # change this to your own miniconda / miniforge path
 source /Users/shaokaiye/miniforge3/bin/activate
-conda env create -f conda/amadesuGPT-minimal.yml
-conda activate amadeusgpt-minimal
+conda env create -f conda/amadesuGPT.yml
+conda activate amadeusgpt
 pip install pycocotools
 pip install -e .[streamlit]
-python -m ipykernel install --user --name amadeusgpt-minimal --display-name "amadeusgpt-minimal"
+python -m ipykernel install --user --name amadeusgpt --display-name "amadeusgpt"
