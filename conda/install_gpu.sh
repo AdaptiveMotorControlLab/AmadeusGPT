a7ae1c4be747b7307939c6a0373730e34d6c52c5
shaokai <shaokaiyeah@gmail.com>
2024-07-30 10:22:53 +0200

    corrected broken links and broken references (#46)
    * corrected broken links and broken references
    
    * Update README.md
    
    - putting /conda only works for linux users so please do not add that @yeshaokai
    
    * Update README.md
    
    - always add full links; in external docs or on pypi these are also broken links @yeshaokai
    
    * Update amadesuGPT.yml
    
    - keep amadeusgpt as a dependency ...
    
    ---------
    
    Co-authored-by: Mackenzie Mathis <mathis@rowland.harvard.edu>

--- conda/install_gpu.sh
+++ conda/install_gpu.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 source /mnt/md0/shaokai/miniconda3/bin/activate
-conda env create -f conda/amadesuGPT-gpu.yml
-conda activate amadeusgpt-gpu
+conda env create -f conda/amadesuGPT.yml
+conda activate amadeusgpt
 # adjust this line according to your cuda version
 conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
 pip install "git+https://github.com/DeepLabCut/DeepLabCut.git@pytorch_dlc#egg=deeplabcut"
 pip install pycocotools
 pip install -e .[streamlit]
 # install the python kernel
-python -m ipykernel install --user --name amadeusgpt-gpu --display-name "amadeusgpt-gpu"
+python -m ipykernel install --user --name amadeusgpt --display-name "amadeusgpt"
